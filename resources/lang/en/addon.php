d3ee858efbf54345c01d9ce3c8785f851f4e2d0d
Mike <sales@enlivenapp.com>
2015-03-06 15:30:33 -0500

    initial commit

---
+++ resources/lang/en/addon.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'name'        => 'BootswatchDarklyTheme',
+    'description' => 'A Bootswatch Darkly theme for PyroCMS v3.'
+];
---
+++ src/BootswatchDarklyTheme.php
@@ -0,0 +1,16 @@
+<?php namespace Enlivenapp\BootswatchDarklyTheme;
+
+use Anomaly\Streams\Platform\Addon\Theme\Theme;
+
+/**
+ * Class BootswatchFlatlyTheme
+ *
+ * @link          http://enlivenapp.com
+ * @author        Enliven Applications <sales@enlivenapp.com>
+ * @author        Michael Webber <mw@enlivenapp.com>
+ * @package       Enlivenapp/bootstrapThreeTheme
+ */
+class BootswatchDarklyTheme extends Theme
+{
+
+}
